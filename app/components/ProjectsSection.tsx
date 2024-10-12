9a0a41fbe925fc15268955139e2a3d6c17d845df
yigitbaris <bariskaynakoglu01@gmail.com>
2024-10-13 00:15:30 +0300

    projects section and slider animation added

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { BsGithub, BsArrowUpRightSquare } from 'react-icons/bs'
+import SlideUp from './SlideUp'
 
 const projects = [
   {
@@ -48,37 +49,41 @@ const ProjectsSection = () => {
         {projects.map((project, idx) => {
           return (
             <div key={idx}>
-              <div className='flex flex-col md:flex-row md:space-x-12'>
-                <div className='md:w-1/2'>
-                  <Image
-                    src={project.image}
-                    alt=''
-                    width={1000}
-                    height={1000}
-                    className='rounded-xl shadow-xl hover:opacity-70'
-                  />
-                </div>
-                <div className='md:w-1/2'>
-                  <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
-                  <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400 '>
-                    {project.description}
-                  </p>
-                  <div className='flex flex-row align-bottom space-x-4'>
-                    <Link href={project.github} target='_blank'>
-                      <BsGithub
-                        size={30}
-                        className='hover:-translate-y-1 transition-transform cursor-pointer'
-                      />
-                    </Link>
+              <SlideUp offset='-300px 0px -300px 0px'>
+                <div className='flex flex-col animate-slideUpCubiBezier animationDelay-2 md:flex-row md:space-x-12'>
+                  <div className='mt-12 md:w-1/2'>
                     <Link href={project.link} target='_blank'>
-                      <BsArrowUpRightSquare
-                        size={30}
-                        className='hover:-translate-y-1 transition-transform cursor-pointer'
+                      <Image
+                        src={project.image}
+                        alt=''
+                        width={1000}
+                        height={1000}
+                        className='rounded-xl shadow-xl hover:opacity-70'
                       />
                     </Link>
                   </div>
+                  <div className='mt-12 md:w-1/2'>
+                    <h1 className='text-4xl font-bold mb-6'>{project.name}</h1>
+                    <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400 '>
+                      {project.description}
+                    </p>
+                    <div className='flex flex-row align-bottom space-x-4'>
+                      <Link href={project.github} target='_blank'>
+                        <BsGithub
+                          size={30}
+                          className='hover:-translate-y-1 transition-transform cursor-pointer'
+                        />
+                      </Link>
+                      <Link href={project.link} target='_blank'>
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className='hover:-translate-y-1 transition-transform cursor-pointer'
+                        />
+                      </Link>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </SlideUp>
             </div>
           )
         })}

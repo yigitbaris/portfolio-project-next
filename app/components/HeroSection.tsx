461c85d5f444c7b144dba3f8ef8278245a77cd9a
yigitbaris <bariskaynakoglu01@gmail.com>
2024-10-12 21:46:36 +0300

    arrow animation

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
           offset={-100}
           duration={500}
         >
-          <HiArrowDown size={35} />
+          <HiArrowDown size={35} className='animate-bounce' />
         </Link>
       </div>
     </section>
